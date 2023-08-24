91d7c7e0a104bb92439e52c5b4dd05b82ab53fd8
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2023-08-24 21:16:29 +0600

    code refractored

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import cors from 'cors'
 import express, { Application } from 'express'
-import userRoutes from '../src/app/modules/users/user.route'
 import globalErrorHandler from './app/middleware/globalErrorHandler'
+import { UserRoutes } from './app/modules/users/user.route'
 const app: Application = express()
 
 app.use(cors())
@@ -11,10 +11,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 //Application routes
-app.use('/api/v1/users/', userRoutes)
+app.use('/api/v1/users/', UserRoutes)
 
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Working')
+//testing api
+// app.get('/', (req: Request, res: Response, next: NextFunction) => {
+//   // res.send('Working')
+//   throw new ApiError(400, 'Custom Error')
 // })
 
 //global error handler
