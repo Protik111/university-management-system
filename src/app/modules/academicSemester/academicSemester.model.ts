a6d9a813d62360c910c3c1be4904ad408fcd1513
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2023-10-24 13:29:20 +0600

    [DONE] semester title and code validation with pre-hook added

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,4 +1,7 @@
+import httpStatus from 'http-status'
 import { Schema, model } from 'mongoose'
+
+import ApiError from '../../../errors/ApiError'
 import {
   academicSemesterCodes,
   academicSemesterTitles,
@@ -38,6 +41,19 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
   },
   { timestamps: true },
 )
+
+//pre-hook for handling same title
+academicSemesterSchema.pre('save', async function (next) {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  })
+
+  if (isExist) {
+    throw new ApiError(httpStatus.CONFLICT, 'Academic is already exist!')
+  }
+  next()
+})
 
 export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   'AcademicSemester',
